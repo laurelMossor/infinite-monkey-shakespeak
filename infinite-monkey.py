1600c29db678b0650b8eb3900dda1c53197edfa4
Laurel Mossor <laurel.mossor@gmail.com>
2022-06-14 14:33:15 -0700

    Works as intended! But on 50% correct after 10mil attempts

--- infinite-monkey.py
+++ infinite-monkey.py
@@ -20,25 +20,72 @@ ALPHABET_LIST = alphabet_list(ALPHABET_SPACE)
 
 def get_phrase():
     """Asks for user input string 
-    and returns a length"""
+    Returns the string and its length"""
 
     human_string = input("What phrase would you like the monkey to generate?\n>").lower()
     print(f"You put '{human_string}'... on it!")
 
-    return len(human_string)
+    return human_string
 
-def generate_string(length):
+def generate_string(human_string):
     """Generate a string that is as long as intended phrase"""
 
     monkey_string = ""
-    for i in range(length):
+    for i in range(len(human_string)):
         monkey_string += choice(ALPHABET_LIST)
     
-    print(monkey_string)
+    # print(monkey_string)
+    return monkey_string
+
+def score_monkey_string(monkey_string, human_string):
+    """Takes in the human string and compares it to the monkey/generated string
+    Returns a score out of 100"""
+
+    # Compare each index to each other and if the same, increase counter
+
+    correct = 0
+    out_of = len(human_string)
+
+    for i in range(len(human_string)):
+        if monkey_string[i] == human_string[i]:
+            correct += 1
+
+    monkey_score = (correct/out_of)*100
+
+    # print(monkey_score)
+    return monkey_score
+
+def dance_monkey(human_string):
+    """Repeatedly call generate and score, then if 100% of the letters are correct we are done. 
+    If the letters are not correct then we will generate a whole new string. 
+    Print out the best string generated so far and its score every 1000 tries."""
+
+    num_attempts = 0
+    best_match_yet = generate_string(human_string)
+    best_score_yet = score_monkey_string(best_match_yet, human_string)
+
+    while best_score_yet != 100:
+
+        num_attempts += 1
+        monkey_string = generate_string(human_string)
+        score = score_monkey_string(monkey_string, human_string)
+
+        if score > best_score_yet:
+            best_score_yet = score
+            best_match_yet = monkey_string
+
+        if num_attempts % 1000 == 0:
+            print(num_attempts)
+            print(best_score_yet)
+            print(best_match_yet)
+    
+    print(f"WOO HOO! After {num_attempts}, the monkey generated {best_match_yet}")
+
+
 
 def MAIN():
-    monkey_string_length = get_phrase()
-    generate_string(monkey_string_length)
+    human_string = get_phrase()
+    dance_monkey(human_string)
 
 
 MAIN()
